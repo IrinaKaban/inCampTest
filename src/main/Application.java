3633b613cc0995853436ea0ca9d0f301ba5847bc
Iryna Kaban <irina.kaban7@gmail.com>
2019-06-06 22:36:36 +0300

    Add list of internship's students in Application + Add students

--- src/main/Application.java
+++ src/main/Application.java
@@ -3,15 +3,23 @@ package main;
 import institution.University;
 import institution.interlink.Internship;
 import person.Student;
+import person.consciousness.Knowledge;
 
 public class Application {
-    public static void main(String[] args) {
-        University university = new University("CH.U.I.");
-        university.addStudent(new Student("Andrew Kostenko"));
-        university.addStudent(new Student("Julia Veselkina"));
-        university.addStudent(new Student("Maria Perechrest"));
+    public static void main(String[] args) throws Exception {
+        University chpt = new University("CH.P.T.");
 
-        Internship internship = new Internship("Interlink");
+        chpt.addStudent(new Student("Andrew Kostenko", new Knowledge(5)));
+        chpt.addStudent(new Student("Julia Veselkina", new Knowledge(8)));
+        chpt.addStudent(new Student("Maria Perechrest", new Knowledge(7)));
+        chpt.addStudent(new Student("Trevor Porter", new Knowledge(7)));
+        chpt.addStudent(new Student("Joan Potter", new Knowledge(3)));
+        chpt.addStudent(new Student("Prudence Brooks", new Knowledge(1)));
+        chpt.addStudent(new Student("William Stark", new Knowledge(4)));
+
+        Internship internship = new Internship("Interlink", chpt);
+        internship.addStudents();
+        
         System.out.println("List of internship's students:");
         System.out.println(internship.getStudents());
     }
